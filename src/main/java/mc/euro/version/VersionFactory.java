c42e9e617128085e872c51b4d977a04e48d69e8f
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-05-20 11:19:53 -0500

    Deprecated, doc'd, future-proofed getNmsVersion().
    Problem:
    The whole problem is that versioned packaging may not exist in future
    versions of Spigot. Also, it does not exist in really old versions of
    Craftbukkit (older than 1.4.5).
    
    So, this update is a safe-guard against possible future changes in Spigot
    while maintaining backwards compatibility. In other words, getNmsVersion()
    will check for the existence of net.minecraft.server.vX_Y_RZ. Previously,
    if this failed, we simply assumed that it's an old version and we returned
    the value "pre"... meaning that it's prior to versions packaging.
    
    Another problem is the return value. The main goal of this method is to
    reliably detect the server version and construct a String "vX_Y_Z" that
    can be used as a package name to access:
    
    com.name.project.nms.vX_Y_Z;
    com.name.project.compat.vX_Y_Z;
    
    Therefore, the real return value should be String, NOT a Version object.
    
    Deprecated:
    getNmsVersion in favor or private getNmsPackage().
    
    Future updates v2.0:
    getNmsVersion() will be removed.
    getNmsPackage() will be public.
    getNmsPackage() will no longer return "pre"
    
    Documented:
    The javadocs @return now document that getNmsVersion() returns "pre" for
    servers less than 1.4.5.

--- src/main/java/mc/euro/version/VersionFactory.java
+++ src/main/java/mc/euro/version/VersionFactory.java
@@ -38,16 +38,75 @@ public class VersionFactory {
     }
     
     /**
-     * Factory method to conveniently construct a Version object of net.minecraft.server.v1_X_RY package. <br/>
+     * Factory method to conveniently construct a Version object from net.minecraft.server.vX_Y_RZ package. <br/>
+     * <b>These return values should NOT be used for comparison.</b><br/>
+     * This should only be used to reliably access your own .compat. packages.
+     * Really, the return value never should have been a Version object
+     * because it just gets immediately converted to a String anyways.
+     * @return - "pre" if the server is less than v1.4.5, otherwise returns "vX_Y_Z"
+     * @deprecated - Scheduled for deletion in favor of getNmsPackage() + getCompatPackage().
      */
+    @Deprecated
     public static Version getNmsVersion() {
         String NMS = null;
             try {
+                // This fails for versions less than 1.4.5
                 NMS = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
             } catch (ArrayIndexOutOfBoundsException ex) {
-                NMS = "pre";
+                NMS = createCompatPackage();
+                // Do what I say, not what I do :P
+                // 1.4.5+ should never reach this point, thus comparison is safe here.
+                // This adds backwards compatibility while future proofing
+                // against possible changes in Spigot.
+                // Note that comparison of 1.4.7 (v1_4_R1) to 1.4.6 (v1_4_6) would normally fail.
+                if (new Version(NMS).compareTo("1.4.5") < 0) {
+                    return new Version("pre");
+                }
             }
         return new Version(NMS);
+    }
+    
+    /**
+     * If net.minecraft.server.vX_Y_RZ package exists, then use that, 
+     * otherwise, call getCompatPackage() to construct vX_Y_Z based on 
+     * the server version that's running.
+     * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
+     */
+    private static String getNmsPackage() {
+        return getNmsVersion().toString();
+    }
+    
+    /**
+     * getNmsPackage() is preferable because it will check the server to see 
+     * if net.minecraft.server.vX_Y_RZ exists and return that. 
+     * getNmsPackage() will also construct vX_Y_Z if it fails (if the package 
+     * n.m.s.v does not exist). However, use getCompatPackage() if you want 
+     * to ignore the existence of the n.m.s.v package. Note that
+     * the n.m.s.v package does not exist in really old versions of Craftbukkit.
+     * Also, it may not exist in future versions of Spigot.
+     * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
+     */
+    private static String getCompatPackage() {
+        return createCompatPackage();
+    }
+    
+    /**
+     * Creates a String for compat packages.
+     * <pre>
+     * For example, if the server is running 1.2.5-R0.1-SNAPSHOT, 
+     * then the return value will be "v1_2_5".
+     * Which can be used to get classes from the package:
+     * com.name.project.compat.v1_2_5;
+     * </pre>
+     * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
+     */
+    private static String createCompatPackage() {
+        String fullVersion = Bukkit.getBukkitVersion();
+        String[] parts = fullVersion.split("[-]");
+        String version = parts[0];
+        version = version.replace(".", "_");
+        version = "v" + version;
+        return version;
     }
     
 }
