a20808065878d4d28657ae362235c837cfa8e625
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-08-05 03:41:21 -0500

    Added IPlugin abstraction
    for the purpose of determining plugin.isEnabled() on different platforms,
    like Bukkit &/or Sponge.
    
        public abstract boolean isEnabled();
    
    This is all the information that we care about for Version checking:
    Because, if the plugin is not enabled, then our compatibility checks
    should fail (as intended).

---
+++ src/main/java/mc/euro/version/plugin/IPlugin.java
@@ -0,0 +1,16 @@
+package mc.euro.version.plugin;
+
+/**
+ * What is IPlugin ?
+ * 
+ * IPlugin is an abstraction of Plugin for the purpose of determining isEnabled().
+ * Once constructed, this object should be passed to a Predicate Tester, 
+ * which will call isEnabled().
+ * 
+ * @author Nikolai
+ */
+public abstract class IPlugin {
+    
+    public abstract boolean isEnabled();
+
+}
