9712a3575a70060d7ecea8b62bb5e888fdc32d07
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-08-05 03:23:14 -0500

    Heavily refactored Tester
    Tester is now a class container for a Predicate tester and T testee:
    
    where test() merely returns
    
    predicate.test(testee);
    
    Therefore, any uses of the old Tester interface should be replaced with
    the new Predicate interface.

--- src/main/java/mc/euro/version/Tester.java
+++ src/main/java/mc/euro/version/Tester.java
@@ -1,15 +1,28 @@
 package mc.euro.version;
 
 /**
- * The Tester interface is used to optionally pass an additional isEnabled() check to the Version object. <br/><br/>
+ * The Predicate Tester is used to optionally pass an additional isEnabled() check to the Version object. <br/><br/>
+ * 
+ * It is a wrapper for the predicate and the object to be tested:
+ * in other words, Tester.test() merely uses predicate.test(object).
  * 
  * Notice that if the test returns false, then the compatibility check will stop, (as intended) 
  * because if the plugin is not enabled, then it's not compatible.
  * 
  * @author Nikolai
  */
-public interface Tester<T> {
+public class Tester<T> {
     
-    public boolean isEnabled(T t);
+    Predicate<T> predicate;
+    T object;
+    
+    public Tester(Predicate<T> tester, T testee) {
+        this.predicate = tester;
+        this.object = testee;
+    }
+    
+    public boolean test() {
+        return predicate.test(object);
+    }
     
 }
