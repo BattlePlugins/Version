a46b1910a3f548b4fa254a6055d25f68d3f217dd
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-08-05 04:34:28 -0500

    VersionFactory is now Platform agnostic
    - This is achieved by powering each VersionFactory method() with
      Platform.getPlatform(), which detects the platform at runtime, then
      fowards the calls to that Platform.
    
    - VersionFactory also exposes two public static final Platform constants:
      BUKKIT + SPONGE
    
      that can be used in areas where you are certain which platform you're on
    
    For example, if your plugin is supporting multiple platforms, like Bukkit
    & Sponge... then you'll most likely have a BukkitMain and a SpongeMain:
    
    Therefore,
    - VersionFactory.BUKKIT can safely be used in BukkitMain
    - VersionFactory.SPONGE can safely be used in SpongeMain
    
    For areas of your plugin that are platform independent, you can use:
    
    VersionFactory.getPluginVersion(pluginName);
    VersionFactory.getMinecraftVersion();
    VersionFactory.getApiVersion();
    VersionFactory.getImplementationVersion();
    VersionFactory.getNmsPackage();
    VersionFactory.getPlatform();
    
    Notice however, that these methods could fail if getPlatform() returns
    Platform.UNKNOWN_PLATFORM, and invoking any further methods() will cause an
    UnsupportedOperationException("Unknown Platform not supported.");
    
    Altho, I think this is extremely unlikely, because you would specifically
    have to add support for your plugin be able to be loaded by these unknown
    platforms.

--- src/main/java/mc/euro/version/VersionFactory.java
+++ src/main/java/mc/euro/version/VersionFactory.java
@@ -1,103 +1,49 @@
 package mc.euro.version;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
+import mc.euro.version.internal.Platform;
 
 /**
- * Handles the construction of new Version objects specific to Craftbukkit.
+ * Handles the construction of new Version objects for BUKKIT & SPONGE.
  * @author Nikolai
  */
 public class VersionFactory {
     
-    /**
-     * Factory method used when you want to construct a Version object via a Plugin object. <br/>
-     */
-    public static Version getPluginVersion(Plugin plugin) {
-        String version = (plugin == null) ? "" : plugin.getDescription().getVersion();
-        Tester<Plugin> tester = TesterFactory.getNewTester(plugin);
-        return new Version<Plugin>(version, tester, plugin);
-    }
+    public static final Platform BUKKIT = Platform.BUKKIT;
+    public static final Platform SPONGE = Platform.SPONGE;
     
     /**
      * Factory method used when you want to construct a Version object via pluginName. <br/>
      */
     public static Version getPluginVersion(String pluginName) {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin(pluginName);
-        String version = (plugin == null) ? "" : plugin.getDescription().getVersion();
-        Tester<Plugin> tester = TesterFactory.getNewTester(plugin);
-        return new Version<Plugin>(version, tester, plugin);
+        return Platform.getPlatform().getPluginVersion(pluginName);
     }
     
     /**
-     * Factory method to conveniently construct a Version object of the server. <br/>
+     * Factory method to conveniently construct a Version object from the Minecraft server. <br/>
      */
-    public static Version getServerVersion() {
-        String version = Bukkit.getServer().getBukkitVersion();
-        return new Version(version);
+    public static Version getMinecraftVersion() {
+        return Platform.getPlatform().getMinecraftVersion();
+    }
+    
+    public static Version getApiVersion() {
+        return Platform.getPlatform().getApiVersion();
+    }
+    
+    public static Version getImplementationVersion() {
+        return Platform.getPlatform().getImplementationVersion();
     }
     
     /**
-     * Factory method to conveniently construct a Version object from net.minecraft.server.vX_Y_RZ package. <br/>
-     * <b>These return values should NOT be used for comparison.</b><br/>
-     * This should only be used to reliably access your own .compat. packages.
-     * Really, the return value never should have been a Version object
-     * because it just gets immediately converted to a String anyways.
-     * @return - "vX_Y_Z". No longer returns "pre" for really old versions of Minecraft.
-     * @deprecated - in favor of getNmsPackage() + getCompatPackage().
-     */
-    @Deprecated
-    public static Version getNmsVersion() {
-        String NMS = null;
-            try {
-                // This fails for versions less than 1.4.5
-                NMS = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                NMS = createCompatPackage();
-            }
-        return new Version(NMS);
-    }
-    
-    /**
-     * If net.minecraft.server.vX_Y_RZ package exists, then use that, 
-     * otherwise, call getCompatPackage() to construct vX_Y_Z based on 
-     * the server version that's running.
+     * Factory method to get the net.minecraft.server.vX_Y_RZ package.
+     * This will return simply "sponge" on the SpongePlatform.
      * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
      */
     public static String getNmsPackage() {
-        return getNmsVersion().toString();
+        return Platform.getPlatform().getNmsPackage();
     }
     
-    /**
-     * getNmsPackage() is preferable because it will check the server to see 
-     * if net.minecraft.server.vX_Y_RZ exists and return that. 
-     * getNmsPackage() will also construct vX_Y_Z if it fails (if the package 
-     * n.m.s.v does not exist). However, use getCompatPackage() if you want 
-     * to ignore the existence of the n.m.s.v package. Note that
-     * the n.m.s.v package does not exist in really old versions of Craftbukkit.
-     * Also, it may not exist in future versions of Spigot.
-     * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
-     */
-    public static String getCompatPackage() {
-        return createCompatPackage();
-    }
-    
-    /**
-     * Creates a String for compat packages.
-     * <pre>
-     * For example, if the server is running 1.2.5-R0.1-SNAPSHOT, 
-     * then the return value will be "v1_2_5".
-     * Which can be used to get classes from the package:
-     * com.name.project.compat.v1_2_5;
-     * </pre>
-     * @return a String in the form of "vX_Y_Z" where "v" is constant and X, Y, & Z are integers.
-     */
-    private static String createCompatPackage() {
-        String fullVersion = Bukkit.getBukkitVersion();
-        String[] parts = fullVersion.split("[-]");
-        String version = parts[0];
-        version = version.replace(".", "_");
-        version = "v" + version;
-        return version;
+    public static Platform getPlatform() {
+        return Platform.getPlatform();
     }
     
 }
